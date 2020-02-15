1f2c8861d420b7c71d4ae72b3b6ca805033c133e
unknown <t.drain@hotmail.com>
2020-02-15 18:10:13 +0000

    Initial re-write as oracle sql

--- scripts/data_storage/Band.sql
+++ scripts/data_storage/Band.sql
@@ -1,11 +1,26 @@
-create table metalarchives.Band (
-    Band_RecordID bigint primary key not null auto_increment,
-    BandID bigint not null, #foreign
-    Name varchar(255),
-    Link varchar(255),
-    Status varchar(255),
-    Country varchar(255),
-    Genre varchar(255),
-    Letter varchar(5) not null,
-    Band_ScrapeID bigint
+CREATE SEQUENCE band_seq START WITH 1;
+
+CREATE TABLE Band (
+    Band_RecordID   NUMBER(10)          NOT NULL,
+    BandID          NUMBER(10)          NOT NULL, --foreign
+    Name            VARCHAR2(255),
+    Link            VARCHAR2(255),
+    Status          VARCHAR2(255),
+    Country         VARCHAR2(255),
+    Genre           VARCHAR2(255),
+    Letter          VARCHAR2(5)      NOT NULL,
+    Band_ScrapeID   NUMBER(10)
 );
+
+ALTER TABLE Band ADD (
+    CONSTRAINT Band_RecordID PRIMARY KEY (Band_RecordID));
+    
+CREATE OR REPLACE TRIGGER band_trg
+BEFORE INSERT ON Band
+FOR EACH ROW
+
+BEGIN
+    SELECT band_seq.NEXTVAL
+    INTO :new.Band_RecordID
+    FROM Dual;
+END;
