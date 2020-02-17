2d4a4c79a746a6dbc0b0db9bb11c2bb97feb7de5
unknown <t.drain@hotmail.com>
2020-02-17 16:22:55 +0000

    General tidy up and simplifying

--- scripts/data_collection/tidy_band.py
+++ scripts/data_collection/tidy_band.py
@@ -2,23 +2,29 @@ from bs4 import BeautifulSoup
 import re
 
 
-def tidy_band(input, log_natural_key):
-    namesoup = [BeautifulSoup(text, 'html.parser') for text in input['Link']]
+def tidy_band(raw_dat, letter):
 
-    input['Link'] = [soup.a['href'] for soup in namesoup]
-    input['BandID'] = [re.sub(".+/(.+)$", "\\1", link) for link in input['Link']]
-    input['Name'] = [soup.a.string for soup in namesoup]
+    # Pull apart the attributes of interest from the link field
+    link_soup = [BeautifulSoup(text, 'html.parser') for text in raw_dat['link']]
+    raw_dat['link'] = [soup.a['href'] for soup in link_soup]
+    raw_dat['name'] = [soup.a.string for soup in link_soup]
 
-    statussoup = [BeautifulSoup(text, 'html.parser') for text in input['Status']]
-    input['Status'] = [soup.span.string for soup in statussoup]
+    raw_dat['bandid'] = [int(re.sub(".+/(.+)$", "\\1", link)) for link in raw_dat['link']]
+
+    # Pull apart the attribute of interest ('Status') from the link field
+    status_soup = [BeautifulSoup(text, 'html.parser') for text in raw_dat['status']]
+    raw_dat['status'] = [soup.span.string for soup in status_soup]
 
     # Store the letter with the band (this is the natural key of the band log table),
     # so we can match this table to band log and add in the scrape ID
-    input['Letter'] = log_natural_key
+    raw_dat['letter'] = letter
 
-    output = input[['BandID', 'Name', 'Link', 'Status', 'Country', 'Genre', 'Letter']]
+    # This will be filled in in the main script
+    raw_dat['band_scrapeid'] = None
+
+    tidy_dat = raw_dat[['bandid', 'name', 'link', 'status', 'country', 'genre', 'letter', 'band_scrapeid']]
     # Current index corresponds to index in smaller chunks concatenated
     # Reset index to start at 0 and end at number of bands
-    output.index = range(len(output))
+    tidy_dat.index = range(len(tidy_dat))
 
-    return output
+    return tidy_dat
