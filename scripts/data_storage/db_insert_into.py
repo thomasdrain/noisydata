e7b74957d000ce7bc404c2ac8391f825762196a8
unknown <t.drain@hotmail.com>
2020-02-24 14:11:33 +0000

    Fix up character encoding to Oracle

--- scripts/data_storage/db_insert_into.py
+++ scripts/data_storage/db_insert_into.py
@@ -1,4 +1,13 @@
+from sqlalchemy import types
 
 
 def db_insert_into(new_rows, table, engine, operation='append'):
-    new_rows.to_sql(table, con=engine, if_exists=operation, index=False, chunksize=10000)
+
+    # Update the datatypes to be inserted into DB, for all 'object' (AKA character) types in the table.
+    # This ensures they don't get turned into CLOB type in Oracle, and use VARCHAR instead
+    # (using max length of the text).
+
+    new_data_types = {c: types.VARCHAR(new_rows[c].str.len().max())
+                      for c in new_rows.columns[new_rows.dtypes == 'object'].tolist()}
+
+    new_rows.to_sql(table, con=engine, if_exists=operation, index=False, chunksize=10000, dtype=new_data_types)
