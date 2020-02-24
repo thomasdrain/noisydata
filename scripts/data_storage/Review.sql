ddd5b81b228b7f21150d34dedbd2c68e24092070
unknown <t.drain@hotmail.com>
2020-02-24 23:45:29 +0000

    Update to PL SQL

--- scripts/data_storage/Review.sql
+++ scripts/data_storage/Review.sql
@@ -1,14 +1,29 @@
-create table metalarchives.Review (
-    Review_RecordID bigint primary key not null auto_increment,
-    #ReviewID bigint, #foreign it appears there isnt a review ID as such...
-    BandID bigint, #foreign
-    AlbumID bigint, #foreign
-    Username varchar(255), #foreign
-    ReviewDate datetime,
-    ReviewLink varchar(500),
-    ReviewScore int,
-    #not storing these in sql currently
-    #ReviewTitle varchar(255),
-    #ReviewContent varchar(255),
-    Review_ScrapeID bigint #foreign
+DROP TABLE REVIEW;
+DROP SEQUENCE REVIEW_SEQ;
+
+CREATE SEQUENCE review_seq START WITH 1;
+
+CREATE TABLE REVIEW (
+    REVIEW_RECORDID     NUMBER(10)      NOT NULL,
+    BANDID              NUMBER(10)      NOT NULL,
+    ALBUMID             NUMBER(10)      NOT NULL,
+    USERNAME            VARCHAR2(255)   NOT NULL,
+    REVIEWDATE          DATE            NOT NULL,
+    REVIEWLINK          VARCHAR2(255),
+    REVIEWSCORE         INT,
+    REVIEW_SCRAPEID     NUMBER(10)
 );
+
+ALTER TABLE REVIEW ADD (
+    CONSTRAINT REVIEW_RECORDID PRIMARY KEY (REVIEW_RECORDID)
+);
+
+CREATE OR REPLACE TRIGGER review_trg
+BEFORE INSERT ON REVIEW
+FOR EACH ROW
+
+BEGIN
+    SELECT review_seq.NEXTVAL
+    INTO :new.Review_RecordID
+    FROM Dual;
+END;
