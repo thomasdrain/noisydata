ea0ee02b6f11354cd35c5d33aa5980520b13a73a
unknown <t.drain@hotmail.com>
2020-04-12 19:18:33 +0100

    Minor update

--- scripts/data_collection/get_url.py
+++ scripts/data_collection/get_url.py
@@ -7,7 +7,7 @@ useragent_header = {'User-Agent': 'Mozilla/5.0'}
 
 # header: The default used by noisydata is Mozilla, which prevents a 403 error.
 # see https://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping
-def get_url(url, payload = None, header = useragent_header):
+def get_url(url, payload=None, header=useragent_header):
 
     if payload != None:
         r = requests.get(url, params=payload, headers=header)
