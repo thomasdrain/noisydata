2417b8fe56cd33812208e054aa64af1c54ae3684
unknown <t.drain@hotmail.com>
2020-04-17 20:31:03 +0100

    Archive old schema

--- scripts/data_storage/BandAlbumsLog.sql
+++
@@ -1,8 +0,0 @@
-create table metalarchives.BandAlbumsLog (
-    BandAlbums_ScrapeID bigint primary key not null auto_increment,
-    BandID bigint, #foreign
-    ScrapeDate datetime,
-    EndDate datetime,
-    Completed varchar(10),
-    Active varchar(10)    
-);
