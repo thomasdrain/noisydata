11b6a300eb081add5e9f9bf57abee76d0f0699dc
unknown <t.drain@hotmail.com>
2020-11-22 22:33:44 +0000

    Use file database alias

--- scripts/data_storage/sqlserver/letter_counts.sql
+++ scripts/data_storage/sqlserver/letter_counts.sql
@@ -1,7 +1,7 @@
 /* count how many records stored per scrape*/
   select l.ScrapeDate, l.Band_ScrapeID, l.Letter, count(b.Band_ScrapeID) N
-  from band b
-  full join bandlog l on b.Band_ScrapeID = l.Band_ScrapeID
+  from metalarchives.dbo.Band b
+  full join metalarchives.dbo.bandlog l on b.Band_ScrapeID = l.Band_ScrapeID
   group by l.ScrapeDate, l.Band_ScrapeID, l.Letter 
   order by l.ScrapeDate, l.Band_ScrapeID, l.Letter
   ;
