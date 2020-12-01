1a1de5485d82957cca9a07e0f496a0d3524867a8
unknown <t.drain@hotmail.com>
2020-12-01 22:24:09 +0000

    Fix python -m call

--- scripts/rundeck/run_album_scraper.sh
+++ scripts/rundeck/run_album_scraper.sh
@@ -1,6 +1,6 @@
 cd /home/ubuntu/noisydata
 . venv/bin/activate
 cd scripts/
-python3 -m data_collection.album_scraper.py
+python3 -m data_collection.album_scraper
 deactivate
 cd /home/ubuntu
--- scripts/rundeck/run_band_scraper.sh
+++ scripts/rundeck/run_band_scraper.sh
@@ -1,6 +1,6 @@
 cd /home/ubuntu/noisydata
 . venv/bin/activate
 cd scripts/
-python3 -m data_collection.band_scraper.py
+python3 -m data_collection.band_scraper
 deactivate
 cd /home/ubuntu
